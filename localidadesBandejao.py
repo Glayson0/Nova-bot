7d244b73bfc7ca651e5f9eb32905ab3c06c2cc54
yellowisk <heitor.a@aluno.ifsp.edu.br>
2024-04-15 01:47:16 -0300

    add bandejao locations and its opening hours

---
+++ localidadesBandejao.py
@@ -0,0 +1,16 @@
+## Dicionários com os horários de refeição do bandejão
+cafeLocalidades = {
+    'RU': ['07:30','08:30']
+}
+
+almocoLocalidades = {
+    'RU': ['10:30','14:00'],
+    'RA': ['11:15','14:00'],
+    'RS': ['11:00','14:00']
+}
+
+jantarLocalidades = {
+    'RU': ['17:30','19:45'],
+    'RA': ['17:30','19:00'],
+    'RS': ['17:30','19:00']
+}
