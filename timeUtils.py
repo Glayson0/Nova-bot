8561ff9e91d90f8111e0cad0a6d6085a10823eeb
Glayson Oliveira <gn.btista@gmail.com>
2024-08-01 13:44:14 -0300

    Add formating function for time difference between times

--- timeUtils.py
+++ timeUtils.py
@@ -5,7 +5,7 @@ Esse arquivo contém todas as funções relacionadas à manipulação de tempo.
 from datetime import datetime, timedelta
 
 # Funções para as variáveis globais
-def getTranslatedWeekday(weekday:str) -> str:
+def translateWeekday(weekday:str) -> str:
     """Traduz o dia da semana do inglês para português brasileiro."""
 
     translation_map = {
@@ -23,15 +23,15 @@ def getTranslatedWeekday(weekday:str) -> str:
 def getDayType(day:str) -> str:
     """Retorna se o dia é útil ou é não-util."""
     if day in 'Segunda Terça Quarta Quinta Sexta':
-        return "dia util"
-    return "dia não-util"
+        return "working day"
+    return "non working day"
 
 # Variáveis globais
 global CURRENT_TIME
 CURRENT_TIME = datetime.now()  # tipo: datetime (YYYY-MM-DD HH:MM:SS.mmmmm)
 
 global CURRENT_WEEKDAY
-CURRENT_WEEKDAY = getTranslatedWeekday(CURRENT_TIME.strftime('%A'))
+CURRENT_WEEKDAY = translateWeekday(CURRENT_TIME.strftime('%A'))
 
 global CURRENT_DAY_TYPE
 CURRENT_DAY_TYPE = getDayType(CURRENT_WEEKDAY)
@@ -48,7 +48,7 @@ def strToDatetime(h:str) -> datetime:
     h = datetime.strptime(h, '%H:%M')
     return datetime(CURRENT_TIME.year, CURRENT_TIME.month, CURRENT_TIME.day, h.hour, h.minute)
 
-def getTimeDifference(startTime, endTime) -> datetime:
+def calculateTimeDifference(startTime, endTime, formated=True) -> datetime.delta:
     """Essa função calcula a diferença de tempo entre dois horários do tipo datetime"""
 
     if type(startTime) == str:
@@ -56,4 +56,20 @@ def getTimeDifference(startTime, endTime) -> datetime:
     if type(endTime) == str:
         endTime = strToDatetime(endTime)
 
-    return startTime - timedelta(hours=endTime.hour, minutes=endTime.minute)
+    timeDelta = startTime - timedelta(hours=endTime.hour, minutes=endTime.minute)
+
+    if formated:
+        if timeDelta.hour > 0:
+            return f'{timeDelta.hour} hr e {timeDelta.minute} min'
+        else:
+            return f'{timeDelta.minute} min'
+    else:
+        return timeDelta
+
+def formatingBusDiffTime(horaAtual, diffTime):
+    
+    if horaAtual != None:
+        if diffTime.hour > 0:
+            return f'{diffTime.hour} hr e {diffTime.minute} min'
+        else:
+            return f'{diffTime.minute} min'
