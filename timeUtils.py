f6b0331fb6f815e290ea2bf35bd3d2221fe07a1d
Glayson Oliveira <gn.btista@gmail.com>
2024-08-05 19:30:42 -0300

    Refatoração de variáveis e remoção da função formatingBusDiffTime()

--- timeUtils.py
+++ timeUtils.py
@@ -27,36 +27,42 @@ def getDayType(day:str) -> str:
     return "non working day"
 
 # Variáveis globais
+global CURRENT_DATETIME
+CURRENT_DATETIME = datetime.now()  # tipo: datetime (YYYY-MM-DD HH:MM:SS.mmmmm)
+
 global CURRENT_TIME
-CURRENT_TIME = datetime.now()  # tipo: datetime (YYYY-MM-DD HH:MM:SS.mmmmm)
+CURRENT_TIME = CURRENT_DATETIME.time()  # tipo: datetime (YYYY-MM-DD HH:MM:SS.mmmmm)
 
 global CURRENT_WEEKDAY
-CURRENT_WEEKDAY = translateWeekday(CURRENT_TIME.strftime('%A'))
+CURRENT_WEEKDAY = translateWeekday(CURRENT_DATETIME.strftime('%A'))
 
 global CURRENT_DAY_TYPE
 CURRENT_DAY_TYPE = getDayType(CURRENT_WEEKDAY)
 
 # Funções
-def datetimeToStr(h: datetime) -> str:
+def datetimeToStr(time: datetime) -> str:
     """Converte variáveis do tipo datetime para string no formato '%H:%M'"""
+    return datetime.strftime(time, '%H:%M')
 
-    return datetime.strftime(h, '%H:%M')
-
-def strToDatetime(h:str) -> datetime:
+def strToTime(time:str):
     """Essa função converte horários do tipo str no formato '%H:%M' para datetime no formato 'ano-mês-dia hora:minuto'"""
+    time = datetime.strptime(time, '%H:%M')
+    return datetime.time(time)
 
-    h = datetime.strptime(h, '%H:%M')
-    return datetime(CURRENT_TIME.year, CURRENT_TIME.month, CURRENT_TIME.day, h.hour, h.minute)
+def strToDatetime(time:str):
+    """Essa função converte horários do tipo str no formato '%H:%M' para datetime no formato 'ano-mês-dia hora:minuto'"""
+    time = datetime.strptime(time, '%H:%M')
+    return datetime(CURRENT_DATETIME.year, CURRENT_DATETIME.month, CURRENT_DATETIME.day, time.hour, time.minute)
 
-def calculateTimeDifference(startTime, endTime, formated=True) -> datetime.delta:
+def calculateTimeDifference(endTime, startTime, formated=True) -> datetime:
     """Essa função calcula a diferença de tempo entre dois horários do tipo datetime"""
 
-    if type(startTime) == str:
-        startTime = strToDatetime(startTime)
     if type(endTime) == str:
         endTime = strToDatetime(endTime)
+    if type(startTime) == str:
+        startTime = strToDatetime(startTime)
 
-    timeDelta = startTime - timedelta(hours=endTime.hour, minutes=endTime.minute)
+    timeDelta = endTime - timedelta(hours=startTime.hour, minutes=startTime.minute)
 
     if formated:
         if timeDelta.hour > 0:
@@ -65,11 +71,3 @@ def calculateTimeDifference(startTime, endTime, formated=True) -> datetime.delta
             return f'{timeDelta.minute} min'
     else:
         return timeDelta
-
-def formatingBusDiffTime(horaAtual, diffTime):
-    
-    if horaAtual != None:
-        if diffTime.hour > 0:
-            return f'{diffTime.hour} hr e {diffTime.minute} min'
-        else:
-            return f'{diffTime.minute} min'
