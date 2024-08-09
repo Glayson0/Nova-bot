62103dbc2baa83091916834ff7b832b8eea23445
Sergio Pires <s248476@dac.unicamp.br>
2024-08-09 14:55:08 -0300

    Improvements in bus_timer

--- bus.py
+++ bus.py
@@ -1,93 +1,24 @@
 """
 Esse arquivo contém todas as funções relacionadas aos ônibus da Unicamp.
 """
+import datetime as dt
+
 from busSchedule import *
 from time_utils import *
 
+
 # Funções auxiliares
-
-def getDayBusSchedule(weekday:str=CURRENT_WEEKDAY) -> tuple:
-    """Retorna uma tupla com as listas de ida e volta dos ônibus dependendo do dia atual."""
-    day_type = is_business_day(weekday)
-    day_type = "business day" if day_type else "weekend"
-
-    if weekday == 'domingo':
-        return dayTypes[day_type][0][:25], dayTypes[day_type][1][:25]
-
-    return dayTypes[day_type][0], dayTypes[day_type][1]
-
-def hasAvailableBus(time:datetime, busSchedule:list) -> bool:
-    """Checa se há ônibus disponível no dia após o horário em que a função foi chamada."""
-    return time < busSchedule[-1]
-
-def nextBusFromNow(busSchedule:list, time:datetime=CURRENT_DATETIME) -> datetime:
+def next_bus_from_now() -> tuple[str, str]:
     """Retorna o horário em datetime do próximo ônibus a partir do horário em que a função foi chamada."""
-    if hasAvailableBus(CURRENT_DATETIME, busSchedule):
-        for busTime in busSchedule:
-            if time <= busTime:
-                return busTime
+    day_type = is_business_day(dt.datetime.now().weekday())
+    day_type = "business_day" if day_type else "weekend"
 
-    return None
-
-def getAvailableBusSchedule(busSchedule:list, time:datetime=CURRENT_DATETIME) -> list:
-    """Retorna uma lista com apenas os ônibus disponíveis no dia a partir do horário em que a função foi chamada."""
-    availableBusSchedule = []
-
-    for busTime in busSchedule:
-        if time >= busTime:
-            availableBusSchedule.append(busTime)
-
-    return availableBusSchedule
+    return BUS_SCHEDULER[day_type]
 
 # Funções principais
-
 def createNextBusMessage() -> str:
-    """Cria uma string com os horários dos conjuntos ida-volta dos ônibus da moradia."""
-    departureBusSchedule, returnBusSchedule = getDayBusSchedule(CURRENT_WEEKDAY)
-
-    # Obtenção dos próximo 2 horários dos ônibus
-    nextDepartureBusTime1 = nextBusFromNow(departureBusSchedule)
-    nextDepartureBusTime2 = nextBusFromNow(departureBusSchedule, nextDepartureBusTime1)
-    nextReturnBusTime1 = nextBusFromNow(returnBusSchedule)
-    nextReturnBusTime2 = nextBusFromNow(returnBusSchedule, nextReturnBusTime1)
-
-    # Tempo restante para os horários - IDA
-    if nextReturnBusTime1:
-        timeForNextDepartureBus1 = calc_time_diff(nextDepartureBusTime1, CURRENT_DATETIME)
-        if nextReturnBusTime2:
-            timeForNextDepartureBus2 = calc_time_diff(nextDepartureBusTime2, CURRENT_DATETIME)
-        else:
-            timeForNextDepartureBus2 = None
-    else:
-        timeForNextDepartureBus1 = None
-        timeForNextDepartureBus2 = None
-
-    # Tempo restante para os horários - VOLTA
-    if nextReturnBusTime1:
-        timeForNextReturnBus1 = calc_time_diff(nextReturnBusTime1, CURRENT_DATETIME)
-        if nextReturnBusTime2:
-            timeForNextReturnBus2 = calc_time_diff(nextReturnBusTime1, CURRENT_DATETIME)
-        else:
-            timeForNextReturnBus2 = None
-    else:
-        timeForNextReturnBus1 = None
-        timeForNextReturnBus2 = None
-
-    # Substituir horários com valor None por "Acabaram os ônibus por hoje!"
-    times = [
-        [dt_to_str(nextDepartureBusTime1), timeForNextDepartureBus1],
-        [dt_to_str(nextDepartureBusTime2), timeForNextDepartureBus2],
-        [dt_to_str(nextReturnBusTime1), timeForNextReturnBus1],
-        [dt_to_str(nextReturnBusTime2), timeForNextReturnBus2]
-    ]
-
-    timesOutput = []
-    for time in times:
-        if time[0] == None:
-            time = r"Acabaram os ônibus por hoje\!"
-        else:
-            time = f"{time[0]} \({time[1]}\)"
-        timesOutput.append(time)
+    # TODO
+    # Pode implementar o resto com a nova lógica :p
 
     # String formatada
     next2busText = f"""
@@ -96,7 +27,7 @@ def createNextBusMessage() -> str:
 ⌚ Horário atual: {dt_to_str(CURRENT_DATETIME)}
 
 ➡️ IDA \(Moradia \-\> Unicamp\):
-    01\) \) \) \) \) \) \) \) \) \) \) \) \) \) \) \) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) {timesOutput[0]}
+    01\) \) \) \) \) \) \) \) \) \) \) \) \) \) \) \) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) ) ) \) {timesOutput[0]}
     02\) {timesOutput[1]}
 
 ⬅️ VOLTA \(Unicamp \-\> Moradia\):
@@ -107,37 +38,7 @@ def createNextBusMessage() -> str:
 
 def createAvailableBusListMessage(busSchedule:list) -> str:
     """Cria uma string com todos os horários do dia de ônibus, riscando os horários que já passaram e destacando o próximo."""
-
-    nextBus = nextBusFromNow(busSchedule)
-    availableBusScheduleListText = "LISTA DE ÔNIBUS DISPONÍVEIS\n\n"
-
-    pos = 0
-    for time in busSchedule:
-
-        # Ônibus que já passaram
-        if time < nextBus:
-            time = dt_to_str(time)
-            pos += 1
-            if pos % 3 == 0:
-                availableBusScheduleListText += f'~{time}~\n'
-            else:
-                availableBusScheduleListText += f'~{time}~  \|  '
-
-        # Próximo ônibus
-        elif time == nextBus:
-            pos += 1
-            if pos % 3 == 0:
-                availableBusScheduleListText += f'*{dt_to_str(nextBus)}*\n'
-            else:
-                availableBusScheduleListText += f'*{dt_to_str(nextBus)}*  \|  '
-
-        # Ônibus que ainda não passaram
-        else:
-            time = dt_to_str(time)
-            pos += 1
-            if pos % 3 == 0:
-                availableBusScheduleListText += f'{time}\n'
-            else:
-                availableBusScheduleListText += f'{time}  \|  '
-
-    return availableBusScheduleListText
+    # BUG
+    # Retornar toda a lista de onibus que ja foram não vale a pena
+    # É muita informação e provavelmente o usuario não quer
+    # Mas pode reeimplementar isso se quiser usando a nova lógica
--- busSchedule.py
+++ busSchedule.py
@@ -1,52 +1,104 @@
 """
 Esse arquivo contém os horários de funcionamento dos ônibus da Unicamp e suas rotas.
 """
-from time_utils import str_to_time
+import dataclasses as dc
+import datetime as dt
+from collections import deque
 
-BUS_FULL_SCHEDULE_PHOTO = 'https://i.pinimg.com/originals/cf/ad/04/cfad0446abaf264bf013c3f391276aac.jpg'
+BUS_FULL__PHOTO = 'https://i.pinimg.com/originals/cf/ad/04/cfad0446abaf264bf013c3f391276aac.jpg'
+MINUTES_IN_A_DAY = 1440
+
+@dc.dataclass
+class BusSchedule:
+    next_departure: str
+    next_return: str
+    remaining_buses: tuple[str] = dc.field()
+    is_weekend: bool = dc.field(default=False)
+    is_first_bus: bool = dc.field(default=False)
+    is_last_bus: bool = dc.field(default=False)
+
+    def __str__ (self) -> str:
+        return self.__repr__()
+    
+    def __repr__ (self) -> str:
+        return (
+            f"Onibus das {self.next_departure} às {self.next_return}"
+            if self.next_departure is not None else r"Acabaram os ônibus por hoje\!"
+    )
 
 # Horários dos ônibus
 
 # Dia útil
-weekdayBusDepartureSchedule = [
-    '06:30', '06:45', '06:50', '07:00', '07:10', '07:15', '07:20', '07:25', '07:35', '07:40', '07:45', '08:00', '08:10', '08:20',
-    '08:30', '08:40', '08:50', '09:00', '09:10', '09:20', '09:30', '09:40', '09:45', '10:05', '10:15', '10:30','10:45', '11:00',
-    '11:20', '11:30', '11:45', '12:00', '12:15', '12:20', '12:35', '12:45', '13:00', '13:05', '13:20', '13:30', '13:45', '14:00',
-    '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:30', '17:45',
-    '18:00', '18:10','18:20', '18:25', '18:30', '18:40', '18:55', '19:00', '19:15', '19:25', '19:35', '19:50', '19:55', '20:10',
-    '20:30', '20:45'
-]
+weekdayBusDeparture = deque((
+    '06:30', '06:45', '06:50', '07:00', '07:10', '07:15', '07:20', '07:25', '07:35', '07:40', '07:45', '08:00',
+    '08:10', '08:20', '08:30', '08:40', '08:50', '09:00', '09:10', '09:20', '09:30', '09:40', '09:45', '10:05',
+    '10:15', '10:30','10:45', '11:00', '11:20', '11:30', '11:45', '12:00', '12:15', '12:20', '12:35', '12:45',
+    '13:00', '13:05', '13:20', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30',
+    '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:30', '17:45', '18:00', '18:10','18:20', '18:25',
+    '18:30', '18:40', '18:55', '19:00', '19:15', '19:25', '19:35', '19:50', '19:55', '20:10', '20:30', '20:45'
+))
 
-weekdayBusReturnSchedule = [
-    '07:20', '07:40', '08:00', '08:25', '08:45', '09:00', '09:30', '09:50', '10:00', '10:15', '10:40', '11:05', '11:15', '11:30', '11:45', '11:55', '12:00',
-    '12:20', '12:30', '12:45', '12:50', '13:00', '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45',
-    '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:15', '17:30', '17:40', '17:45',
-    '17:50', '18:00', '18:15', '18:20', '18:30', '18:40', '18:50', '19:05', '19:10', '19:25', '19:35', '19:45',
-    '20:05', '20:20', '20:40', '20:50', '21:00', '21:25', '21:35', '21:55', '22:00', '22:10', '22:20', '22:30',
-    '22:35', '22:45', '23:05', '23:15', '23:25', '23:35', '23:45'
-]
+weekdayBusReturn = deque((
+    '07:20', '07:40', '08:00', '08:25', '08:45', '09:00', '09:30', '09:50', '10:00', '10:15', '10:40', '11:05',
+    '11:15', '11:30', '11:45', '11:55', '12:00', '12:20', '12:30', '12:45', '12:50', '13:00', '13:15', '13:30',
+    '13:45', '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30',
+    '16:45', '17:15', '17:30', '17:40', '17:45', '17:50', '18:00', '18:15', '18:20', '18:30', '18:40', '18:50',
+    '19:05', '19:10', '19:25', '19:35', '19:45', '20:05', '20:20', '20:40', '20:50', '21:00', '21:25', '21:35',
+    '21:55', '22:00', '22:10', '22:20', '22:30', '22:35', '22:45', '23:05', '23:15', '23:25', '23:35', '23:45'
+))
 
 # Dia não-útil
-nonWorkingDayBusDepartureSchedule = [
-    '07:10', '07:20', '07:30', '07:40', '07:50', '08:00', '08:10', '08:20', '11:00', '11:10', '11:20',
-    '11:30', '11:40', '11:50', '12:00', '12:10', '12:20', '12:30', '12:40', '12:50', '13:00', '13:10', 
-    '13:20', '13:30', '13:40', '13:50', '17:30', '17:40', '17:50', '18:00', '18:10', '18:20', '18:30',
-    '18:40', '18:50'
-]
+nonWorkingDayBusDeparture = deque((
+    '07:10', '07:20', '07:30', '07:40', '07:50', '08:00', '08:10', '08:20', '11:00',
+    '11:10', '11:20', '11:30', '11:40', '11:50', '12:00', '12:10', '12:20', '12:30',
+    '12:40', '12:50', '13:00', '13:10',  '13:20', '13:30', '13:40', '13:50', '17:30',
+    '17:40', '17:50', '18:00', '18:10', '18:20', '18:30', '18:40', '18:50'
+))
 
-nonWorkingDayBusReturnSchedule = [
-    '07:20', '07:30', '07:40', '07:50', '08:00', '08:10', '08:20', '08:30', '11:10', '11:20', '11:30', '11:40', 
-    '11:50', '12:00', '12:10', '12:20', '12:30', '12:40', '12:50', '13:00', '13:10', '13:20', '13:30', '13:40',
-    '13:50', '14:00', '17:40', '17:50', '18:00', '18:10', '18:20', '18:30', '18:40', '18:50', '19:00'
-]
+nonWorkingDayBusReturn = deque((
+    '07:20', '07:30', '07:40', '07:50', '08:00', '08:10', '08:20', '08:30', '11:10',
+    '11:20', '11:30', '11:40',  '11:50', '12:00', '12:10', '12:20', '12:30', '12:40',
+    '12:50', '13:00', '13:10', '13:20', '13:30', '13:40', '13:50', '14:00', '17:40',
+    '17:50', '18:00', '18:10', '18:20', '18:30', '18:40', '18:50', '19:00'
+))
 
-# Conversão dos horários em datetime
-weekdayBusDepartureSchedule = list(map(str_to_time, weekdayBusDepartureSchedule))
-weekdayBusReturnSchedule = list(map(str_to_time, weekdayBusReturnSchedule))
-nonWorkingDayBusDepartureSchedule = list(map(str_to_time, nonWorkingDayBusDepartureSchedule))
-nonWorkingDayBusReturnSchedule = list(map(str_to_time, nonWorkingDayBusReturnSchedule))
+def create_bus_schedule(
+    departure: deque[str], return_: deque[str], is_weekend: bool = False
+) -> dict[str, BusSchedule]:
+    """
+    Cria um dicionário com os horários dos ônibus.
+    """
+    bus_schedule = {}
 
-dayTypes = {
-    "business day": (weekdayBusDepartureSchedule, weekdayBusReturnSchedule),
-    "weekend": (nonWorkingDayBusDepartureSchedule, nonWorkingDayBusReturnSchedule)
+    counter = dt.datetime.strptime("00:00", "%H:%M")
+    next_bus = ( departure.popleft(), return_.popleft() )
+    is_first_bus = True
+    is_last_bus = False
+
+    for _ in range(MINUTES_IN_A_DAY):
+        time_as_str = counter.strftime("%H:%M")
+
+        # BUG o return_ deveria ser sempre o mais próximo possível do departure
+        if time_as_str == next_bus[0]:
+            if len(departure):
+                next_bus = ( departure.popleft(), return_.popleft() )
+
+                if len(departure) == 0:
+                    is_last_bus = True
+            
+            else:
+                next_bus = ( None, None )
+
+        bus_schedule[time_as_str] = BusSchedule(
+            *next_bus, tuple(departure), is_weekend, is_first_bus, is_last_bus
+        )
+        is_first_bus = False
+
+        counter += dt.timedelta(minutes=1)
+
+    return bus_schedule
+
+BUS_SCHEDULER = {
+    "bussines_day": create_bus_schedule(weekdayBusDeparture, weekdayBusReturn),
+    "weekend": create_bus_schedule(nonWorkingDayBusDeparture, nonWorkingDayBusReturn, True)
 }
