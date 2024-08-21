acbe2df6c226b9c9e466ccf3b0d3522f959b3d67
Glayson Oliveira <gn.btista@gmail.com>
2024-08-21 16:06:16 -0300

    Add is_time_valid and is_weekday_valid functions

--- time_utils.py
+++ time_utils.py
@@ -1,13 +1,31 @@
 """Esse arquivo contém todas as funções relacionadas à manipulação de tempo.
 """
-# Use locale to set the default locale
-# that converts literals to portuguese
 
-def is_business_day (day: int) -> bool:
+from re import match
+
+
+def is_business_day(day: int) -> bool:
     """Receives days as int, Monday = 0, Tuesday = 1, ..., Sunday = 6
-       and Returns True if it is a business day, False otherwise
+    and Returns True if it is a business day, False otherwise
     """
     return day < 5
 
-# BUG Esse modulo não tem muita utilidade,
-# tudo o que ele faz é ter uma função que ja ta implementado no datetime
+
+def is_time_valid(time: str) -> bool:
+    time_pattern = r"^\d{2}:\d{2}$"
+    is_valid_time_pattern = match(time_pattern, time)
+
+    if not is_valid_time_pattern:
+        return False
+
+    hours, minutes = map(int, time.split(':'))
+    is_valid_time_range = 0 <= hours < 24 and 0 <= minutes < 60
+
+    if not is_valid_time_range:
+        return False
+    
+    return True
+
+
+def is_weekday_valid(weekday: int) -> bool:
+    return 0 <= weekday <= 9
