0937c7cd773d84e4ff870e8a1ae83ac21f90fc5b
Glayson Oliveira <gn.btista@gmail.com>
2024-08-21 00:03:57 -0300

    Refactor BusSchedule class to separate departure and return remaining buses

--- bus_schedule.py
+++ bus_schedule.py
@@ -12,7 +12,8 @@ MINUTES_IN_A_DAY = 1440
 class BusSchedule:
     next_departure: str
     next_return: str
-    remaining_buses: tuple[str] = dc.field()
+    remaining_dep_buses: tuple[str] = dc.field()
+    remaining_ret_buses: tuple[str] = dc.field()
     is_weekend: bool = dc.field(default=False)
     is_first_bus: bool = dc.field(default=False)
     is_last_bus: bool = dc.field(default=False)
@@ -40,7 +41,7 @@ business_day_departure_list = deque((
     '08:10', '08:20', '08:30', '08:40', '08:50', '09:00', '09:10', '09:20', '09:30', '09:40', '09:45', '10:05',
     '10:15', '10:30', '10:45', '11:00', '11:20', '11:30', '11:45', '12:00', '12:15', '12:20', '12:35', '12:45',
     '13:00', '13:05', '13:20', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30',
-    '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:30', '17:45', '18:00', '18:10','18:20', '18:25',
+    '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:30', '17:45', '18:00', '18:10', '18:20', '18:25',
     '18:30', '18:40', '18:55', '19:00', '19:15', '19:25', '19:35', '19:50', '19:55', '20:10', '20:30', '20:45'
 ))
 
@@ -63,7 +64,7 @@ weekend_departure_list = deque((
 
 weekend_return_list = deque((
     '07:20', '07:30', '07:40', '07:50', '08:00', '08:10', '08:20', '08:30', '11:10',
-    '11:20', '11:30', '11:40',  '11:50', '12:00', '12:10', '12:20', '12:30', '12:40',
+    '11:20', '11:30', '11:40', '11:50', '12:00', '12:10', '12:20', '12:30', '12:40',
     '12:50', '13:00', '13:10', '13:20', '13:30', '13:40', '13:50', '14:00', '17:40',
     '17:50', '18:00', '18:10', '18:20', '18:30', '18:40', '18:50', '19:00'
 ))
@@ -104,8 +105,9 @@ def create_bus_schedule(
                 next_return_bus = None
 
         bus_schedule[time_as_str] = BusSchedule(
-            next_departure_bus, next_return_bus, tuple(departure), 
-            is_weekend, is_first_bus, is_last_bus
+            next_departure_bus, next_return_bus, tuple(departure),
+            tuple(return_), is_weekend, is_first_bus,
+            is_last_bus
         )
         is_first_bus = False
 
